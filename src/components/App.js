45f9b2f09b1d339c909e219cb0bdb090eb0ed591
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-10-21 20:26:09 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
+import Para from "./Para";
+
 
 function App() {
+
+  const [para, setPara] = React.useState(null);
+
+  const handelClick = ()=>{
+
+    const newPara = <Para/> 
+    setPara(newPara);
+
+  }
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handelClick}>click</button>
+      {para}
     </div>
   );
 }
-
 
 export default App;
